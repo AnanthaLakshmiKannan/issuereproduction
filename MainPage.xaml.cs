2f010fc249e19e0bfa15f82a3232612667de9ba9
AnanthaLakshmiKannan <92776579+AnanthaLakshmiKannan@users.noreply.github.com>
2023-05-02 20:39:53 +0530

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Scrollv.ScrollToAsync(label20, ScrollToPosition.Start, true);
+    }
+}
+
